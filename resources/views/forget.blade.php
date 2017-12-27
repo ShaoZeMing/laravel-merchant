b8db4d7fd295c146ce8184c8f2489f29daec2f37
shaozeming <szm19920426@gmail.com>
2017-12-27 09:31:49 +0800

    修改username转mobile bug

--- resources/views/forget.blade.php
+++ resources/views/forget.blade.php
@@ -32,15 +32,15 @@
     <p class="login-box-msg">{{ trans('merchant.forget') }}</p>
 
     <form action="{{ merchant_base_path('auth/forget') }}" method="post">
-      <div class="form-group has-feedback {!! !$errors->has('username') ?: 'has-error' !!}">
+      <div class="form-group has-feedback {!! !$errors->has('mobile') ?: 'has-error' !!}">
 
-        @if($errors->has('username'))
-          @foreach($errors->get('username') as $message)
+        @if($errors->has('mobile'))
+          @foreach($errors->get('mobile') as $message)
             <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label></br>
           @endforeach
         @endif
 
-        <input type="input" class="form-control" placeholder="{{ trans('merchant.username') }}" name="username" value="{{ old('username') }}">
+        <input type="input" class="form-control" placeholder="{{ trans('merchant.mobile') }}" name="mobile" value="{{ old('mobile') }}">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback {!! !$errors->has('password') ?: 'has-error' !!}">
